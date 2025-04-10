48eeff6511b93c710448dd117b6d0eb208dc1e3c
Argen SAIPZHANOV <argen.saipzhanov.alt@gmail.com>
2025-04-10 12:12:15 +0300

    Project completed

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 import { menuArray } from "/data.js"
 
+const ordersEl = document.getElementById("orders")
+const footerEl = document.getElementById("footer")
+const modalEl = document.getElementById("modal")
+const inputName = document.getElementById("input-name")
+const modalForm = document.getElementById("modal-form")
+let totalPrice = 0
+
+
+
 document.addEventListener("click", function (e) {
-    if (e.target.id.includes("pizza")) {
-        console.log(e.target.id)
-        handleClick(e.target.id)
+    menuArray.map((item) => {
+        if (e.target.id === item.name) {
+            handleClick(e.target.id)
+        }
+    })
+    if (e.target.id === "remove") {
+        e.target.parentElement.parentElement.remove()
+        if (ordersEl.childElementCount === 0) {
+            footerEl.classList.add("invisible")
+        }
+    }
+
+    if (e.target.id === "complete-btn") {
+        modalEl.classList.remove("invisible")
     }
 })
 
@@ -26,20 +46,35 @@ function render() {
 }
 
 function handleClick(orderId) {
-    const footerEl = document.getElementById("footer")
-    const ordersEl = document.getElementById("orders")
-    footerEl.classList.toggle("invisible")
-    console.log(orderId)
+    const totalPriceEl = document.getElementById("total-price")
+    footerEl.classList.remove("invisible")
 
-    if (menuArray.name === orderId) {
-        ordersEl.innerHTML = `
+    ordersEl.innerHTML += menuArray.map((item) => {
+        if (item.name === orderId) {
+            totalPrice += item.price
+            return `
     <li class="order">
-                <h2 class="order-h2 h2">${orderId} <span role="button" id="remove" class="remove">remove</span></h2>
-                <h3 class="info-price">$${this.price}</h3>
+                <h2 class="order-h2 h2">${item.name} <span role="button" id="remove" class="remove">remove</span></h2>
+                <h3 class="info-price">$${item.price}</h3>
             </li>
     `
-
-    }
+        }
+    }).join("")
+    totalPriceEl.innerHTML = "$" + totalPrice
+    render()
 }
+
+modalForm.addEventListener("submit", function (e) {
+    e.preventDefault()
+    modalEl.remove()
+    footerEl.remove()
+    const congEl = document.querySelector(".cong")
+    congEl.classList.remove("invisible")
+    const modalFormData = new FormData(modalForm)
+    const name = modalFormData.get('name')
+    congEl.innerHTML = `
+    <p class="cong-p">Thanks, ${name}! Your order is on its way!</p>
+    `
+})
 
 render()
